9fef804f5f44339c7856f3aade47a8a800e06dd1
Khalil ZRIBI <107150442+khalilZribi01@users.noreply.github.com>
2024-01-22 19:52:55 +0100

    3nd commit

--- src/main/java/tn/esprit/tic/springproj/services/IEtudiantService.java
+++ src/main/java/tn/esprit/tic/springproj/services/IEtudiantService.java
@@ -1,4 +1,14 @@
 package tn.esprit.tic.springproj.services;
 
+import tn.esprit.tic.springproj.entity.Etudiant;
+
+import java.util.List;
+
 public interface IEtudiantService {
+    List<Etudiant> retrieveAllEtudiants();
+    Etudiant addEtudiant(Etudiant e);
+    Etudiant updateEtudiant(Etudiant e);
+    Etudiant retrieveEtudiant(Long idEtudiant);
+    void removeEtudiant(Long idEtudiant);
+    List<Etudiant> addEtudiants (List<Etudiant> etudiants);
 }
